248bf43c0f444c15348f2982f26ee15ab53e5d75
Bananaandpen <767056404@qq.com>
2021-03-24 15:55:05 +0800

    抽离router，路由切换后aside 关闭

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,19 @@
 </template>
 
 <script lang="ts">
-import { provide, ref } from 'vue'
+import { ref, provide } from 'vue'
+import { router } from './router'
 export default {
   name: 'App',
   setup() {
     const width = document.documentElement.clientWidth
     const asideVisible = ref(width <= 500 ? false : true)
     provide('asideVisible', asideVisible)
+    router.afterEach(() => {
+      if (width <= 500) {
+        asideVisible.value = false
+      }
+    })
   },
 }
 </script>
---
+++ src/components/Tabs.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Tabs</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default {}
+</script>
+
+<style scoped></style>
