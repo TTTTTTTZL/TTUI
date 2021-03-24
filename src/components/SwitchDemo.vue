899f8865c7ebdfc089b89df6851f8aa314ca2d7c
Bananaandpen <767056404@qq.com>
2021-03-24 17:50:48 +0800

    switch css初步提交

---
+++ src/components/SwitchDemo.vue
@@ -0,0 +1,15 @@
+<template>
+  <Switch />
+</template>
+
+<script>
+import Switch from '../lib/Switch.vue'
+
+export default {
+  components: {
+    Switch,
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
