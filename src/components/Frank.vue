8b0ddae333fe1b6730d0a4ddc31528d4593d97fb
Bananaandpen <767056404@qq.com>
2021-03-24 13:23:52 +0800

    初始化vue-router

---
+++ src/components/Frank.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>我的第一个组件</h1>
+</template>
+
+<script>
+export default {
+  name: 'Frank',
+
+  data() {
+    return {}
+  },
+}
+</script>
---
+++ src/components/Frank2.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Frank2</h1>
+</template>
+
+<script>
+export default {
+  name: 'Frank2',
+
+  data() {
+    return {}
+  },
+}
+</script>
