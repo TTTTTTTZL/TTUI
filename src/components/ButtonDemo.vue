bef21c0790813e1aed283b3d00898276308a610d
Bananaandpen <767056404@qq.com>
2021-03-27 15:58:06 +0800

    初始化一个button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,9 +1,21 @@
 <template>
   <div>button</div>
+  <Button @click="click" @mouseover="click" size="small"> aaa</Button>
 </template>
 
-<script>
-export default {}
+<script lang="ts">
+import Button from '../lib/Button.vue'
+export default {
+  components: {
+    Button,
+  },
+  setup() {
+    const click = () => {
+      console.log('hi')
+    }
+    return { click }
+  },
+}
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/lib/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup(props, context) {
+    const attrs = context.attrs
+    console.log(attrs)
+    return { attrs }
+  },
+})
+</script>
+
+<style scoped></style>
